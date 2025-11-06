2ea1b2fa23548d18a450ae538c7d50ecde52035a
Rafael Vieira <devrafaelvieira@gmail.com>
2025-11-06 09:57:08 -0300

    Remove Vision component import from Index.tsx
    Removed import statement for Vision component.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Networks from "@/components/Networks";
-import Vision from "@/components/Vision";
 import Footer from "@/components/Footer";
 import NetworkStatus from "@/components/NetworkStatus";
 
